85cd2df171b9a18ce9c65955bec01d60675fa74d
kevinpatxi <kevin.patxi@gmail.com>
2017-07-21 10:53:10 -0700

    Broke out GPS offset calculator functions

---
+++ GPSOffsetCalculator.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using MissionPlanner;
+
+namespace ITGeoTagger
+{
+    class GPSOffsetCalculator
+    {
+        ITGeotagger Parent_ITGeoTagger;
+        public GPSOffsetCalculator(ITGeotagger parent) {
+            Parent_ITGeoTagger = parent;
+        }//constructor
+
+        public async Task<float> GetImagetoTriggerOffset(string dirPictures, string logFile)
+        {
+            string logFilePath = Path.Combine(dirPictures, logFile);
+            if (!File.Exists(logFilePath))
+            {
+                MessageBox.Show("tlog file " + logFilePath + " does not exist ");
+                return 0;
+            }
+            if (!Directory.Exists(dirPictures))
+            {
+                MessageBox.Show("Image directory " + dirPictures + " does not exist ");
+                return 0;
+            }
+
+            DateTime imsettime = await GetFirstSustainedImageTime(dirPictures);
+            DateTime tmptgtime = await GetFirstSustainedTriggerTime(logFilePath);
+            DateTime tgtime = tmptgtime.AddSeconds(this.Parent_ITGeoTagger.cameraShutterLag);
+            //AppendLogTextBox("\n\nImage time : " + imsettime.ToString());
+            //AppendLogTextBox("\nTrigger time : " + tgtime.ToString());
+
+            float trig2im = (float)(imsettime - tgtime).TotalSeconds;
+            return trig2im;
+        }
+
+        async private Task<DateTime> GetFirstSustainedImageTime(string ImageDir)
+        {
+            DateTime SusImTime = new DateTime();
+            List<string> Allimages = GetAllImagesinDirectory(ImageDir);
+            if (Allimages != null)
+            {
+                string[] Images = Allimages.ToArray(); // create image list array
+                DateTime[] ModTimes = new DateTime[Images.Length];//array of modified times for sorting 
+
+                for (int i = 0; i < Images.Length; i++) // go through each image and get its modified time
+                {
+                    ModTimes[i] = GetDateTakenFromImage(Images[i]);  //new FileInfo(Images[i]).LastWriteTime;
+                }
+                Array.Sort(ModTimes, Images);// sort image list array by time taken
+
+                int cntr = 0;
+
+                DateTime FirstSustained = GetDateTakenFromImage(Images[0]);
+                DateTime LastTime = FirstSustained;
+
+                string ImUsed;
+
+                foreach (string Im in Images)
+                {
+                    DateTime thisTime = GetDateTakenFromImage(Im);
+
+                    if ((thisTime - LastTime).TotalSeconds < 10)
+                    {
+                        if (cntr > 9)
+                        {
+                            //AppendLogTextBox("\n\n Image returned on: " + Im);
+                            return FirstSustained;
+                        }
+                        cntr++;
+                    }
+                    else
+                    {
+                        cntr = 0;
+                        FirstSustained = GetDateTakenFromImage(Im);
+                        ImUsed = Im;
+                        //AppendLogTextBox("\n\n Image used: " + Im);
+                    }
+
+                    LastTime = thisTime;
+                }
+            }
+
+            return SusImTime;
+
+        }
+        async private Task<DateTime> GetFirstSustainedTriggerTime(string fn)
+        {
+            DateTime SusTrigTime = new DateTime();
+
+            Dictionary<long, VehicleLocation> vehicletriggerList = new Dictionary<long, VehicleLocation>();
+            // Telemetry Log
+            if (fn.ToLower().EndsWith("tlog"))
+            {
+
+                System.IO.FileStream logplaybackfile = new System.IO.FileStream(fn, FileMode.Open);
+
+                MAVLinkInterface mine = new MAVLinkInterface(logplaybackfile);
+
+                bool WasLastOn = false;
+
+                MissionPlanner.CurrentState cs = new MissionPlanner.CurrentState();
+
+                while (mine.logplaybackfile.BaseStream.Position < mine.logplaybackfile.BaseStream.Length)
+                {
+                    MAVLink.MAVLinkMessage packet = mine.readPacket();
+
+                    cs.datetime = mine.lastlogread;
+
+                    cs.UpdateCurrentSettings(null, true, mine);
+
+                    VehicleLocation location = new VehicleLocation();
+                    location.Time = cs.datetime;
+                    bool tmponval = false;
+
+                    if (((cs.ch7in > 1520) || (cs.ch7in < 1480)) && (cs.ch7in != 0)) //if ch7 val is not default we assume camera is on
+                    {
+                        tmponval = true;
+                        if (!WasLastOn)
+                        {
+                            SusTrigTime = cs.datetime;
+                        }
+                        else
+                        {
+                            // logic to check if we have a sustained trigger
+                            System.TimeSpan SusTime = cs.datetime - SusTrigTime;
+                            double Sustimesec = SusTime.TotalSeconds;
+
+                            if (Sustimesec > 30)
+                            {
+                                mine.logplaybackfile.Close();
+                                return SusTrigTime;
+                            }
+                        }
+                        WasLastOn = true;
+                    }
+                    else
+                    {
+                        if (cs.ch7in != 0)
+                        {
+                            tmponval = false;
+                            if (WasLastOn)
+                            {
+                                // logic to check if we have a sustained trigger
+                                System.TimeSpan SusTime = cs.datetime - SusTrigTime;
+                                double Sustimesec = SusTime.TotalSeconds;
+
+                                if (Sustimesec > 30)
+                                {
+                                    mine.logplaybackfile.Close();
+                                    return SusTrigTime;
+                                }
+                            }
+                        }
+                        WasLastOn = false;
+                    }
+                }
+                mine.logplaybackfile.Close();
+            }
+            return SusTrigTime;
+        }
+        private static Regex r = new Regex(":");
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                using (Image myImage = Image.FromStream(fs, false, false))
+                {
+                    PropertyItem propItem = myImage.GetPropertyItem(36867);
+                    string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                    return DateTime.Parse(dateTaken);
+                }
+            }
+            catch
+            {
+                return new DateTime();
+            }
+        }
+        private List<string> GetAllImagesinDirectory(string ImageDir)
+        { // get collection of images on the card
+            IEnumerable<string> ImageFiles = Directory.EnumerateFiles(ImageDir, "*.JPG", SearchOption.TopDirectoryOnly);
+            ImageFiles.Concat(Directory.EnumerateFiles(ImageDir, "*.jpg", SearchOption.TopDirectoryOnly)); // adds other file types
+
+            List<string> tmpImageFileList = ImageFiles.ToList();
+            List<string> ImageFileList = new List<string>();
+            //filter out tlog images
+            foreach (string file in tmpImageFileList)
+            {
+                if (!file.Contains("tlog"))
+                {
+                    ImageFileList.Add(file);
+                }
+            }
+            return ImageFileList;
+        }
+    }
+}
--- ITGeotagger.cs
+++ ITGeotagger.cs
@@ -77,15 +77,19 @@ namespace ITGeoTagger
         public Thread PreProcessThread;
         public Thread PostProcessThread;
 
-        private float VerticalImageDistribution = 2;
-        private float cameraShutterLag = (float)1.5;
+        public float VerticalImageDistribution = 2;//not used here but should be
+        public float cameraShutterLag = (float)1.5;
 
         public ITConfigFile appSavedData;
+
+        public GPSOffsetCalculator MY_GPSOffsetCalculator;
+
 
         public ITGeotagger()
         {
             InitializeComponent();
             appSavedData = new ITConfigFile();
+            MY_GPSOffsetCalculator = new GPSOffsetCalculator(this);
             appSavedData.LoadFile();
             //CHECK_AMSLAlt_Use.Checked = true;
             //PANEL_TIME_OFFSET.Enabled = false;
@@ -118,6 +122,8 @@ namespace ITGeoTagger
             TIMER_THREAD_CHECKER.Start();
 
         }
+
+
 
         private async void BUT_GET_DIR_Click(object sender, EventArgs e)
         {
@@ -175,35 +181,13 @@ namespace ITGeoTagger
                     }
                     else
                     {
-                        float tmpTrigTime = await GetImagetoTriggerOffset(folder, GetTlogInDir(folder));
+                        float tmpTrigTime = await MY_GPSOffsetCalculator.GetImagetoTriggerOffset(folder, GetTlogInDir(folder));
                         ATable.AddRow(folder, GetTlogInDir(folder), tmpTrigTime.ToString("G4"), Directory.GetFiles(folder,"*.JPG").Length);
                     }
                 }
             }
         }
-        private async Task<float> GetImagetoTriggerOffset(string dirPictures, string logFile)
-        {
-            string logFilePath = Path.Combine(dirPictures, logFile);
-            if (!File.Exists(logFilePath))
-            {
-                MessageBox.Show("tlog file " + logFilePath + " does not exist ");
-                return 0;
-            }
-            if (!Directory.Exists(dirPictures))
-            {
-                MessageBox.Show("Image directory " + dirPictures + " does not exist ");
-                return 0;
-            }
-
-            DateTime imsettime = await GetFirstSustainedImageTime(dirPictures);
-            DateTime tmptgtime = await GetFirstSustainedTriggerTime(logFilePath);
-            DateTime tgtime = tmptgtime.AddSeconds(this.cameraShutterLag);
-            AppendLogTextBox("\n\nImage time : " + imsettime.ToString());
-            AppendLogTextBox("\nTrigger time : " + tgtime.ToString());
-
-            float trig2im = (float)(imsettime - tgtime).TotalSeconds;
-            return trig2im;
-        }
+        
         private List<string> GetAllImageDirs(string SearchDir)
         {
 
@@ -1097,165 +1081,7 @@ namespace ITGeoTagger
                                              "GeoRefnetworklink.kml");
         }
         private static Regex r = new Regex(":");
-        public static DateTime GetDateTakenFromImage(string path)
-        {
-            try
-            {
-                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
-                using (Image myImage = Image.FromStream(fs, false, false))
-                {
-                    PropertyItem propItem = myImage.GetPropertyItem(36867);
-                    string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
-                    return DateTime.Parse(dateTaken);
-                }
-            }
-            catch
-            {
-                return new DateTime();
-            }
-        }
-        private List<string> GetAllImagesinDirectory(string ImageDir)
-        { // get collection of images on the card
-            IEnumerable<string> ImageFiles = Directory.EnumerateFiles(ImageDir, "*.JPG", SearchOption.TopDirectoryOnly);
-            ImageFiles.Concat(Directory.EnumerateFiles(ImageDir, "*.jpg", SearchOption.TopDirectoryOnly)); // adds other file types
-
-            List<string> tmpImageFileList = ImageFiles.ToList();
-            List<string> ImageFileList = new List<string>();
-            //filter out tlog images
-            foreach (string file in tmpImageFileList)
-            {
-                if (!file.Contains("tlog"))
-                {
-                    ImageFileList.Add(file);
-                }
-            }
-            return ImageFileList;
-        }
-        async private Task<DateTime> GetFirstSustainedImageTime(string ImageDir)
-        {
-            DateTime SusImTime = new DateTime();
-            List<string> Allimages = GetAllImagesinDirectory(ImageDir);
-            if (Allimages != null)
-            {
-                string[] Images = Allimages.ToArray(); // create image list array
-                DateTime[] ModTimes = new DateTime[Images.Length];//array of modified times for sorting 
-
-                for (int i = 0; i < Images.Length; i++) // go through each image and get its modified time
-                {
-                    ModTimes[i] = GetDateTakenFromImage(Images[i]);  //new FileInfo(Images[i]).LastWriteTime;
-                }
-                Array.Sort(ModTimes, Images);// sort image list array by time taken
-
-                int cntr = 0;
-
-                DateTime FirstSustained = GetDateTakenFromImage(Images[0]);
-                DateTime LastTime = FirstSustained;
-
-                string ImUsed;
-
-                foreach (string Im in Images)
-                {
-                    DateTime thisTime = GetDateTakenFromImage(Im);
-
-                    if ((thisTime - LastTime).TotalSeconds < 10)
-                    {
-                        if (cntr > 9)
-                        {
-                            //AppendLogTextBox("\n\n Image returned on: " + Im);
-                            return FirstSustained;
-                        }
-                        cntr++;
-                    }
-                    else
-                    {
-                        cntr = 0;
-                        FirstSustained = GetDateTakenFromImage(Im);
-                        ImUsed = Im;
-                        //AppendLogTextBox("\n\n Image used: " + Im);
-                    }
-
-                    LastTime = thisTime;
-                }
-            }
-
-            return SusImTime;
-
-        }
-        async private Task<DateTime> GetFirstSustainedTriggerTime(string fn)
-        {
-            DateTime SusTrigTime = new DateTime();
-
-            Dictionary<long, VehicleLocation> vehicletriggerList = new Dictionary<long, VehicleLocation>();
-            // Telemetry Log
-            if (fn.ToLower().EndsWith("tlog"))
-            {
-
-                System.IO.FileStream logplaybackfile = new System.IO.FileStream(fn, FileMode.Open);
-
-                MAVLinkInterface mine = new MAVLinkInterface(logplaybackfile);
-
-                bool WasLastOn = false;
-
-                MissionPlanner.CurrentState cs = new MissionPlanner.CurrentState();
-
-                while (mine.logplaybackfile.BaseStream.Position < mine.logplaybackfile.BaseStream.Length)
-                {
-                    MAVLink.MAVLinkMessage packet = mine.readPacket();
-
-                    cs.datetime = mine.lastlogread;
-
-                    cs.UpdateCurrentSettings(null, true, mine);
-
-                    VehicleLocation location = new VehicleLocation();
-                    location.Time = cs.datetime;
-                    bool tmponval = false;
-
-                    if (((cs.ch7in > 1520) || (cs.ch7in < 1480)) && (cs.ch7in != 0)) //if ch7 val is not default we assume camera is on
-                    {
-                        tmponval = true;
-                        if (!WasLastOn)
-                        {
-                            SusTrigTime = cs.datetime;
-                        }
-                        else
-                        {
-                            // logic to check if we have a sustained trigger
-                            System.TimeSpan SusTime = cs.datetime - SusTrigTime;
-                            double Sustimesec = SusTime.TotalSeconds;
-
-                            if (Sustimesec > 30)
-                            {
-                                mine.logplaybackfile.Close();
-                                return SusTrigTime;
-                            }
-                        }
-                        WasLastOn = true;
-                    }
-                    else
-                    {
-                        if (cs.ch7in != 0)
-                        {
-                            tmponval = false;
-                            if (WasLastOn)
-                            {
-                                // logic to check if we have a sustained trigger
-                                System.TimeSpan SusTime = cs.datetime - SusTrigTime;
-                                double Sustimesec = SusTime.TotalSeconds;
-
-                                if (Sustimesec > 30)
-                                {
-                                    mine.logplaybackfile.Close();
-                                    return SusTrigTime;
-                                }
-                            }
-                        }
-                        WasLastOn = false;
-                    }
-                }
-                mine.logplaybackfile.Close();
-            }
-            return SusTrigTime;
-        }
+       
         async private void TagARow(int row)
         {
             string dirPictures = ATable.Table.GetControlFromPosition(0, row).Text;
